3663b52ff50d34eef1ec69aeb7371566b3dc4540
Rohit <39048926+rohsin47@users.noreply.github.com>
2019-03-27 18:49:18 +0800

    Add files via upload

--- src/app/matcher/matcher.component.ts
+++ src/app/matcher/matcher.component.ts
@@ -19,10 +19,12 @@ import { UserDataService } from '../service/user-data.service';
   styleUrls: ['./matcher.component.scss']
 })
 export class MatcherComponent {  
-    score: number;
-    age: number;
-    height: number;
-    result: UserResult;
+    hasPhoto: string;
+    inContact: string;
+    isFavorite: string;
+    compatibilityScore : { lower: number; upper: number; }  = {'lower':1, 'upper':99}
+    age: { lower: number; upper: number; } = {'lower':18, 'upper':95};
+    height: { lower: number; upper: number; } = {'lower':135, 'upper':210};
     users: User[];
     model = new UserFilter();
 
@@ -32,6 +34,20 @@ export class MatcherComponent {
 
   loadUsers() {
     this.users = this.userDataService.getUsers();
+  }
+  
+  filterUsers(event) {
+      this.model.hasphoto = this.hasPhoto;
+      this.model.incontact = this.inContact;
+      this.model.isfavourite = this.isFavorite;
+      this.model.mincompatibilityscore = this.compatibilityScore.lower;
+      this.model.maxcompatibilityscore = this.compatibilityScore.upper;
+      this.model.minimumage = this.age.lower;
+      this.model.maximumage = this.age.upper;
+      this.model.minimumheight = this.height.lower;
+      this.model.maximumheight = this.height.upper;
+      console.log(this.model);
+      this.users = this.userDataService.filterUsers(this.model);
   }
 
 }
--- src/app/service/user-data.service.ts
+++ src/app/service/user-data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import userData from '../../data/matches.json';
 import { User } from '../model/user';
 import { UserFilter } from '../model/user-filter';
-import { UserResult } from '../model/user-result';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs'
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
@@ -24,7 +23,56 @@ export class UserDataService {
   getUsers(): any {  
       return userData;
   }
-
+  
+  filterUsers(filterBy: UserFilter){
+      
+      let query = true;
+      
+      return this.getUsers().filter((item) => {
+          if(filterBy.hasphoto) {
+              if(filterBy.hasphoto == 'yes') {
+                  query = item["main_photo"]; 
+              } else {
+                  query = !item["main_photo"];
+              }
+          }
+          if(filterBy.incontact) {
+              if(filterBy.incontact == 'yes') {
+                  query = query && item["contacts_exchanged"] > 0
+              } else {
+                  query = query && item["contacts_exchanged"] < 0
+              }
+          }
+          if(filterBy.isfavourite){
+              if(filterBy.isfavourite == 'yes') {
+                  query = query && item["favourite"]; 
+              } else {
+                  query = query && !item["favourite"];
+              }
+          }
+          if(filterBy.mincompatibilityscore) {
+              query = query && (item["compatibility_score"] > (filterBy.mincompatibilityscore/100)); 
+          } 
+          if(filterBy.maxcompatibilityscore) {
+              query = query && (item["compatibility_score"] < (filterBy.maxcompatibilityscore/100)); 
+          } 
+          if(filterBy.minimumage) {
+              query = query && (item["age"] > filterBy.minimumage); 
+          } 
+          if(filterBy.maximumage) {
+              query = query && (item["age"] < filterBy.maximumage); 
+          } 
+          if(filterBy.minimumheight) {
+              query = query && (item["height_in_cm"] > filterBy.minimumheight); 
+          } 
+          if(filterBy.maximumheight) {
+              query = query && (item["height_in_cm"] < filterBy.maximumheight); 
+          } 
+          console.log(query);
+          return query;
+      });     
+  }
+  
   queryUsers(params?: any) {
       if(!params) {
         return this.getUsers();
@@ -33,7 +81,10 @@ export class UserDataService {
       return this.getUsers().filter((item) => {
         for(let key in params) {
           let field = item[key];
-          if(typeof field == 'string' && field.toLowerCase().indexOf(params[key].toLowerCase()) > -1) {
+          if(!field){
+             return null;
+          }
+          else if(typeof field == 'string' && field.toLowerCase().indexOf(params[key].toLowerCase()) > -1) {
             return item;
           } else if(field == params[key]) {
             return item;
